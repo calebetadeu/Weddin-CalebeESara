c3a5424642f52f9f25b2c6244d5a91a896de904d
Calebe Tadeu <calebetadeu@gmail.com>
2021-08-29 16:31:36 -0300

    list and present

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -10,37 +10,22 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/freeze.jpg'
-              text='Geladeira'
+              src='images/present.jpg'
+              text='Veja nossa Lista de Presente'
               label='Para Casa'
-              link="https://www.querodecasamento.com.br/lista-de-casamento/calebeesara/checkout/carrinho/"
+              link="https://www.querodecasamento.com.br/lista-de-casamento/calebeesara"
             />
-            <CardItem
-              src='images/Campos.png'
-              text='Diárias'
-              label='Lua de Mel' 
-              link="https://www.youtube.com/watch?v=adLGHcj_fmA"
-            />
-          </ul>
-          <ul className='cards__items'>
             <CardItem
               src='images/nubank.png'
               text='Nos Ajude Ofertando'
               label='Pix'
               link="https://nubank.com.br/pagar/5nbfl/GUZLEPo5jA"
             />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              link="https://www.youtube.com/watch?v=adLGHcj_fmA"
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              link="https://www.youtube.com/watch?v=adLGHcj_fmA"
-            />
+          </ul>
+          <ul className='cards__items'>
+           
+            
+          
           </ul>
         </div>
       </div>
