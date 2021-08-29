730a0d3f66c2bee8608fa75ac976e199bfc6ae1a
Calebe Tadeu <calebetadeu@gmail.com>
2021-08-29 17:45:31 -0300

    list and present

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React, { Component } from "react";
 
 import './contact.css'
@@ -24,7 +25,7 @@ export class Contact extends Component {
               
               <div className="contact-item">
                
-                <iframe className='renovo-map' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15943.745099873126!2d-44.2397944!3d-2.5277062!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd8367395f9c58d8d!2sIgreja%20Evang%C3%A9lica%20Renovo!5e0!3m2!1spt-BR!2sbr!4v1630268612558!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy"></iframe>
+                <iframe className='renovo-map' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15943.745099873126!2d-44.2397944!3d-2.5277062!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd8367395f9c58d8d!2sIgreja%20Evang%C3%A9lica%20Renovo!5e0!3m2!1spt-BR!2sbr!4v1630268612558!5m2!1spt-BR!2sbr" ></iframe>
             
               </div>
               <div className="contact-item">
